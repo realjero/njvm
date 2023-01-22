60b406750797b5a62ff30d71cb3521e469bc0c80
jerome <jerome_jero@t-online.de>
2023-01-22 22:20:37 +0100

    added alloc for allocation on heap

--- heap.c
+++ heap.c
@@ -1,1 +1,13 @@
 #include "heap.h"
+
+void *alloc(unsigned int size) {
+    void *obj;
+    if ((njvm.heap.next + size) <= njvm.heap.end) {
+        obj = njvm.heap.next;
+        njvm.heap.next += size;
+    } else {
+        printf("GarbageCollectneeded\n");
+        // Garbage Collect
+    }
+    return obj;
+}
